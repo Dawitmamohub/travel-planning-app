7137d9f1b3c9839d9d13583fd99a1c136e7a4f5e
Dawit Mamo <Dawitmamoyou@gmail.com>
2025-08-14 13:35:06 +0300

    App.jsx error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,3 @@ export default function App() {
     </div>
   );
 }
-export default function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-100">
-      <h1 className="text-4xl font-bold text-blue-700">
-        Travel Planning App
-      </h1>
-    </div>
-  );
-}
